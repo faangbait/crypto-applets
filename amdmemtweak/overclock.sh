fdc5d9dc05e70e747d90aeb0445f68bc5cc369b9
Sage Smith <sage@limitlessinteractive.com>
2021-02-28 05:28:04 -0600

    amdmemtweak

---
+++ amdmemtweak/overclock.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+/home/ss/Binaries/amdmemorytweak/linux/amdmemtweak --CL 20 --RAS 30 --RCDRD 14 --RCDWR 12 --RC 44 --RP 14 --RRDS 3 --RRDL 6 --RTP 5 --FAW 12 --CWL 8 --WTRS 4 --WTRL 9 --WR 14 --REF 17000 --RFC 249
